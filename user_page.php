fd1774c9106a53cf1c2847ca714497b3b63a0162
yurinjava <yurisouzact@gmail.com>
2023-12-28 21:52:30 -0300

    user page and logout option, fixes #9

--- user_page.php
+++ user_page.php
@@ -8,15 +8,48 @@ if(isset($_SESSION['authentication']) && $_SESSION['authentication']==1){
 
 <head>
     <meta charset="UTF-8">
+    <link rel="icon" href="assets/icons/site-icon.png">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>User Profile - Web Commerce</title>
+    <title>ProtoType - User</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style/user_page.css">
 </head>
 
 <body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top mb-5">
+        <a class="navbar-brand" href="home.php">ProtoType</a>
+        
+            <form class="form-inline w-75 mx-auto" method="get" action="home.php">
+                <input class="form-control mx-auto mr-sm-2 w-50" name="search" type="search" placeholder="<?php  if(isset($_GET['search'])){echo $_GET['search'];}else{ echo 'search';} ?>" aria-label="Search">
+                <input type="hidden" name="action" value="search">
+                <button class="btn my-2 my-sm-0" type="submit"><img src="assets/icons/search-white.png" alt=""></button>
+            </form>
+       
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><i class="fas fa-shopping-cart"></i><img src="assets/icons/shopping-cart-white.png" class="icon"> Cart</a>
+                </li>
 
-    <div class="container mt-5">
-        <h2 class="text-center mb-4">User Profile</h2>
+                <?php
+                if(isset($_SESSION['user_name'])) {
+                    echo " <li class='nav-item'>
+                                <a class='nav-link' href='user_page.php'><i class='fas fa-user'></i><img src='assets/icons/user-white.png' class='icon'>".$_SESSION['user_name']."</a>
+                            </li>";
+                }else{
+                    echo "<li class='nav-item'>
+                            <a class='nav-link' href='login.php'><i class='fas fa-user'></i><img src='assets/icons/enter-white.png' class='icon'>Login</a>
+                        </li>";
+                }
+                ?>
+                
+            </ul>
+        </div>
+    </nav>
+
+    <div class="container ">
+        <h2 class="text-center mb-4"><?php echo "Hello ".$_SESSION['user_name']."!"; ?></h2>
 
         <div class="row">
             <!-- Account Information Option -->
@@ -25,7 +58,7 @@ if(isset($_SESSION['authentication']) && $_SESSION['authentication']==1){
                     <div class="card-body">
                         <h5 class="card-title">Account Information</h5>
                         <p class="card-text">View and update your account details.</p>
-                        <a href="#" class="btn btn-primary">Go to Account</a>
+                        <a href="#" class="btn btn-secondary">Go to Account</a>
                     </div>
                 </div>
             </div>
@@ -36,7 +69,7 @@ if(isset($_SESSION['authentication']) && $_SESSION['authentication']==1){
                     <div class="card-body">
                         <h5 class="card-title">Your Purchases</h5>
                         <p class="card-text">View a history of your purchases.</p>
-                        <a href="#" class="btn btn-primary">View Purchases</a>
+                        <a href="#" class="btn btn-secondary">View Purchases</a>
                     </div>
                 </div>
             </div>
@@ -47,7 +80,7 @@ if(isset($_SESSION['authentication']) && $_SESSION['authentication']==1){
                     <div class="card-body">
                         <h5 class="card-title">Your Address</h5>
                         <p class="card-text">Manage your shipping addresses.</p>
-                        <a href="#" class="btn btn-primary">Manage Addresses</a>
+                        <a href="#" class="btn btn-secondary">Manage Addresses</a>
                     </div>
                 </div>
             </div>
@@ -58,16 +91,17 @@ if(isset($_SESSION['authentication']) && $_SESSION['authentication']==1){
                     <div class="card-body">
                         <h5 class="card-title">Your Shopping Cart</h5>
                         <p class="card-text">View and manage items in your shopping cart.</p>
-                        <a href="#" class="btn btn-primary">View Shopping Cart</a>
+                        <a href="#" class="btn btn-secondary">View Shopping Cart</a>
                     </div>
                 </div>
             </div>
         </div>
+        <a href="home.php" class="btn btn-dark mt-5 mb-5">Return</a>
+        <a href="ecommerce-private/login-control/logout.php" class="btn btn-danger mt-5 mb-5">Logout</a>
+        <footer class="text-center text-secondary"> <p>© 2023 ProtoType. All Rights Reserved.</p></footer>
     </div>
 
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    
 
 </body>
 
