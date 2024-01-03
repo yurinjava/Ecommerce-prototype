55d803ffcd59a16307b52dfcc0187cbe5d76e03f
yurinjava <yurisouzact@gmail.com>
2024-01-02 23:52:59 -0300

    cart troubles pt2

--- ecommerce-private/cart-control/stack-cart.php
+++ ecommerce-private/cart-control/stack-cart.php
@@ -9,14 +9,15 @@ if(!isset( $_SESSION['authentication'])){
 if(isset($_SESSION['products'])){
     foreach ($_SESSION['products'] as $key=> $product) {
         if ($product["id"] == $_POST['product_id']) {
-           unset($_SESSION['products'][$key]); // Remove the element with the matching id
-           echo "<script> alert('teste') </script>";
+           
+           unset($_SESSION['products'][$key]); 
+           //echo "<script> alert('teste') </script>";
         }
     }
 }
-echo '<pre>';
-print_r($_POST);
-echo '</pre>';
+//echo '<pre>';
+//print_r($_POST);
+//echo '</pre>';
 
 
 $productId = $_POST['product_id'];
@@ -24,14 +25,14 @@ $productName = $_POST['product_name'];
 $productImg = $_POST['product_img'];
 $productPrice = $_POST['product_price'];
 $productQuantity = $_POST['product_quantity'];
-// Set values for the object using the __set method
+
 // Store the product in an array
 $_SESSION['products'][] = array(
     'id'   => $productId,
     'name' => $productName,
     'price' => $productPrice,
     'img' => $productImg,
-    'quantity' => null
+    'quantity' => $productQuantity
 );
 
 
